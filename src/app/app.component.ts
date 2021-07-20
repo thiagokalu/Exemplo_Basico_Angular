8818dd1a83bfb1928f43844b8fe10fa390b36ed3
Thiago Stteffen <thiagokalu@gmail.com>
2021-07-20 11:59:09 -0300

    Primeiro commit do projeto de angular da aula de reforço do Youtube

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aulaangular';
+  title = 'Meu Primeiro Projeto Completo de Angular 10';
+  nome = 'Thiago Stteffen';
+  vetor = ['tv', 'airsoft', 'games','fotografia', 'carros']
 }
